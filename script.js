4d3b6962898b18c54a8f6de1faa603e523010652
bercun <bercun@gmail.com>
2025-11-30 13:48:31 +0000

    fix db

--- script.js
+++ script.js
@@ -81,18 +81,6 @@ function setupAddButtons() {
 
 setupAddButtons();
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBPF0mwb2kioMb6SCEAhUwRhpq0sCb9N_U",
-  authDomain: "carbassdeportes.firebaseapp.com",
-  databaseURL: "https://carbassdeportes-default-rtdb.firebaseio.com",
-  projectId: "carbassdeportes",
-  storageBucket: "carbassdeportes.appspot.com",
-  messagingSenderId: "348979037154",
-  appId: "1:348979037154:web:66c7dc823cbd8afb38afbb",
-  measurementId: "G-03K2T3BM9Y"
-};
-
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
